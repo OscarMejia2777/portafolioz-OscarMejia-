6e87175454e09def66e1fc792265d6a2d115c94c
Oscar Mejia <oscar.mejia2777@gmail.com>
2026-05-19 14:31:16 -0600

    refactor: provide SettingsProvider at app root

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/styles/globals.css'
 import { LanguageProvider } from '@/context/LanguageContext'
+import { SettingsProvider } from '@/context/SettingsContext'
+import { AuthProvider } from '@/hooks/useAuth'
 import App from '@/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <AuthProvider>
+      <LanguageProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </LanguageProvider>
+    </AuthProvider>
   </StrictMode>
 )
